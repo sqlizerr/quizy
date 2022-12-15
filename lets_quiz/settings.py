8923f4a2b81f627f64512b93aeff595ee8f7c8cf
Vedang Utekar <utekar.vedang2002@gmail.com>
2022-12-15 13:55:08 +0530

    settings

--- lets_quiz/settings.py
+++ lets_quiz/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (str(os.path.join(BASE_DIR, 'staticfiles')),)
+
 
 hostname = os.environ['DBHOST']
 
